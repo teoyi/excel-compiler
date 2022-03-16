d31f2e71abd8ae15a48ef4d811cdea7f7f00f978
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-03-16 21:14:48 +0800

    clean up

--- MERGE_DAILY_SOT.py
+++ MERGE_DAILY_SOT.py
@@ -3,22 +3,14 @@ import pandas as pd
 
 FILE_PATH = "C:\\Users\\yipen\\Desktop\\SOT - MAR 2022 1-14.xlsx"
 xl = pd.ExcelFile(FILE_PATH)
-# print(xl.sheet_names)
 
 required_sheets = []
 for sheet_names in xl.sheet_names:
-    # print(sheet_names.split(' '))
-    # if sheet_names.split(' ')[0].isnumeric() and len(sheet_names.split(' ')) == 3:
-    # print(sheet_names)
     if sheet_names.split(" ")[0].isnumeric() and sheet_names.split(" ")[2].isnumeric():
-        # print(sheet_names)
         required_sheets.append(sheet_names)
 
-# print(required_sheets)
 
 data = []
-# assuming all sheets have the same headers
-# 01 JAN 2022', '02 Jan 2022'
 for required_sheet in required_sheets:
     print(required_sheet)
 for required_sheet in required_sheets:
@@ -26,13 +18,7 @@ for required_sheet in required_sheets:
     sheet["Date"] = required_sheet
     data.append(sheet)
 
-# for datas in data:
-#     print(datas)
 compiled_data = pd.concat(data, axis=0, ignore_index=True)
-# print(compiled_data.dtypes)
-# compiled_data['Service Remarks'] = compiled_data['ServiceComment'].astype(str) + ';' + \
-#     compiled_data['Remark/ Issue (Reschedule/Cancel Date (CCC/CR: date & time inform MGL)'].astype(str)
-# print(compiled_data.columns)
 compiled_data.drop(
     compiled_data.index[compiled_data["DocumentNo"] == "DocumentNo"], inplace=True
 )
